2d73ba6de5b860ec4a9fa680fc9777149ace36c4
abdubekusyimyk <abdybekuulusyimyk@gmail.com>
2023-08-16 20:41:27 +0600

    syimyk

--- lib/modules/home/view/home_page_detail.dart
+++ lib/modules/home/view/home_page_detail.dart
@@ -36,6 +36,38 @@ class _HomePageDetailState extends State<HomePageDetail> {
           title: Text("Продукты")),
       body: Column(
         children: <Widget>[
+          // Padding(
+          //   padding: const EdgeInsets.symmetric(horizontal: 10),
+          //   child: Row(
+          //     children: [
+          //       Padding(
+          //         padding: const EdgeInsets.all(8.0),
+          //         child: SizedBox(
+          //           width: 280,
+          //           height: 50,
+          //           child: TextField(
+          //             onChanged: (value) {
+          //               filterSearchResults(value);
+          //             },
+          //             controller: editingController,
+          //             decoration: InputDecoration(
+          //                 labelText: "Search",
+          //                 hintText: "Search",
+          //                 prefixIcon: Icon(Icons.search),
+          //                 border: OutlineInputBorder(
+          //                     borderRadius:
+          //                         BorderRadius.all(Radius.circular(25.0)))),
+          //           ),
+          //         ),
+          //       ),
+          //       Spacer(),
+          //       Text(
+          //         'Отмена',
+          //         style: TextStyle(fontSize: 20, color: Colors.black),
+          //       )
+          //     ],
+          //   ),
+          // ),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextField(
@@ -65,38 +97,142 @@ class _HomePageDetailState extends State<HomePageDetail> {
           // ),
 
           Expanded(
-            child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                    maxCrossAxisExtent: 200,
-                    childAspectRatio: 2 / 2,
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 15),
-                itemCount: 8,
-                itemBuilder: (BuildContext ctx, index) {
-                  return Container(
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                        color: Colors.amber,
-                        borderRadius: BorderRadius.circular(15)),
-                    child: const Align(
-                      alignment: Alignment.bottomLeft,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                      maxCrossAxisExtent: 200,
+                      childAspectRatio: 3 / 4.3,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 15),
+                  itemCount: 8,
+                  itemBuilder: (BuildContext ctx, index) {
+                    return Container(
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          color: Colors.black12,
+                          borderRadius: BorderRadius.circular(15)),
                       child: Padding(
-                        padding: EdgeInsets.all(10.0),
-                        child: Text(
-                          'Name',
-                          // textAlign: TextAlign.end,
-                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(15.0),
+                              child: Image.asset(
+                                'assets/images/momo.png',
+                                fit: BoxFit.cover,
+                                width: double.infinity,
+                              ),
+                            ),
+                            Text(
+                              'Морковь Нантская',
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 16),
+                            ),
+                            Spacer(),
+                            Text(
+                              '45c',
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 16),
+                            ),
+                            SizedBox(
+                              width: double.infinity,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  showModalBottomSheet(
+                                    context: context,
+                                    elevation: 0,
+                                    useRootNavigator: true,
+                                    isScrollControlled: true,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(15.0),
+                                    ),
+                                    builder: (BuildContext context) {
+                                      return Container(
+                                        padding: EdgeInsets.all(10),
+                                        // margin: EdgeInsetsDirectional.all(10),
+                                        width: double.maxFinite,
+                                        height: 570,
+                                        child: BottomSheetWidget(
+                                          // order: order,
+                                          divider: Divider(
+                                            thickness: 4,
+                                            height: 4,
+                                            indent: 160,
+                                            endIndent: 160,
+                                          ),
+                                          textApp: "rrrrr",
+                                          textStyle: const TextStyle(
+                                              color: Colors.red,
+                                              fontSize: 22,
+                                              fontWeight: FontWeight.w500),
+                                          sizedBox: SizedBox(
+                                            child: ElevatedButton(
+                                              onPressed: () {},
+                                              style: ButtonStyle(
+                                                backgroundColor:
+                                                    MaterialStateColor
+                                                        .resolveWith((states) =>
+                                                            Colors.green),
+                                              ),
+                                              child: Text('Оформить заказ',
+                                                  style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 18,
+                                                  )),
+                                            ),
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                  );
+                                },
+                                child: Text(
+                                  "Добавить",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                style: ElevatedButton.styleFrom(
+                                    primary: Colors.green),
+                              ),
+                            )
+                          ],
                         ),
                       ),
-                    ),
-                  );
-                }),
+                    );
+                  }),
+            ),
           ),
         ],
+      ),
+      floatingActionButton: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+            fixedSize: Size(160, 60), primary: Colors.green),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+            Text(
+              'Корзина 56 с',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
 }
+
+class OrderDetailsScreen {}
 
 class TabBarView extends StatelessWidget {
   const TabBarView({super.key});
@@ -146,11 +282,164 @@ class TabBarView extends StatelessWidget {
             ),
           ]),
     );
-    //   body: TabBarView(children: [
-    //     Icon(Icons.apps),
-    //     Icon(Icons.movie),
-    //     Icon(Icons.games),
-    //   ]),
-    // ));
+  }
+}
+
+class BottomSheetWidget extends StatelessWidget {
+  const BottomSheetWidget({
+    super.key,
+    // required this.order,
+    this.divider,
+    this.textApp,
+    this.textStyle,
+    this.sizedBox,
+  });
+
+  // final Order? order;
+  final Divider? divider;
+
+  final String? textApp;
+  final TextStyle? textStyle;
+  final SizedBox? sizedBox;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            padding: const EdgeInsets.all(8),
+            itemCount: 1,
+            itemBuilder: (context, index) {
+              return const Column(children: [
+                FructList(
+                  imege: 'assets/images/image .png',
+                  title: "Драконий фрукт",
+                  subtitle: "sub",
+                  trailing2: "trai",
+                  trailing: "trail",
+                )
+              ]);
+            },
+          ),
+        ),
+        SizedBox(width: 328, height: 54, child: sizedBox),
+      ],
+    );
+  }
+}
+
+class FructList extends StatelessWidget {
+  const FructList({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.trailing,
+    required this.imege,
+    required this.trailing2,
+  });
+  final String title;
+  final String subtitle;
+  final String trailing;
+  final String trailing2;
+  final String imege;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 150,
+      width: 400,
+      child: Card(
+        color: Colors.blueGrey,
+        margin: const EdgeInsets.symmetric(vertical: 15, horizontal: 3),
+        shape: RoundedRectangleBorder(
+          side: const BorderSide(color: Colors.white38, width: 1),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        elevation: 1,
+        child: Center(
+            child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 7, horizontal: 7),
+                child: Stack(alignment: Alignment.bottomLeft, children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(12.0),
+                    child: Image.asset(
+                      'assets/images/momo.png',
+                      // fit: BoxFit.cover,
+                      // width: 100,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                    width: 40,
+                    child: Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(7)),
+                        color: Colors.white,
+                        child: const Icon(
+                          Icons.delete,
+                          color: Colors.red,
+                        )),
+                  ),
+                ]),
+              ),
+            ),
+            Expanded(
+              child: Column(
+                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Драконий фрукт"),
+                  Text("Драконий фрукт"),
+                  Spacer(),
+                  Row(
+                    children: [
+                      Text("680"),
+                      Spacer(),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Icon(Icons.add, color: Colors.white),
+                        style: ElevatedButton.styleFrom(
+                          shape: CircleBorder(),
+                          padding: EdgeInsets.all(10),
+                          backgroundColor: Colors.blue, // <-- Button color
+                          foregroundColor: Colors.red, // <-- Splash color
+                        ),
+                      ),
+                      Text("2"),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Icon(Icons.add, color: Colors.white),
+                        style: ElevatedButton.styleFrom(
+                          shape: CircleBorder(),
+                          padding: EdgeInsets.all(10),
+                          backgroundColor: Colors.blue, // <-- Button color
+                          foregroundColor: Colors.red, // <-- Splash color
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        )
+
+            // ListTile(
+            //     leading: Image.asset(
+            //       imege,
+            //       // width: 100,
+            //     ),
+            //     title: Text(title),
+            //     subtitle: Text(subtitle),
+            //     trailing: Column(
+            //       children: [Text(trailing), Text(trailing2)],
+            //     )),
+            ),
+      ),
+    );
   }
 }
